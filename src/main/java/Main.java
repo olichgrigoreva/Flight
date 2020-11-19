ee686e4d064ca15157c653a09fddd622d922386f
Olga <aboutrocky94@gmail.com>
2020-11-19 18:47:15 +0300

    add percentile calc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import objectJSON.Response;
-import objectJSON.Ticket;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,26 +11,15 @@ public class Main {
 
             Response response = objectMapper.readValue(Paths.get("tickets.json").toFile(), Response.class);
 
-            SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-
-            Date departure_time = null;
-            Date arrival_time = null;
-
-            List<Long> timeDiffList = new ArrayList<>();
-
-            for (Ticket ticket : response.tickets) {
-                departure_time = format.parse(ticket.getDeparture_time());
-                arrival_time = format.parse(ticket.getArrival_time());
-                //in milliseconds
-                long diff = arrival_time.getTime() - departure_time.getTime();
-                timeDiffList.add(diff);
-            }
-
             System.out.println("Среднее время полета между городами Владивосток и Тель-Авив:");
-            System.out.println(Statistics.timeConverter(Statistics.averageTime(timeDiffList)));
+            Statistics.timeConverter(
+                    Statistics.averageTime(response, "Владивосток", "Тель-Авив"));
 
+            System.out.println("90-й процентиль времени полета между городами Владивосток и Тель-Авив:");
+            Statistics.timeConverter(
+                    Statistics.percentile(response, 90, "Владивосток", "Тель-Авив"));
 
-        } catch (IOException | ParseException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/Statistics.java
+++ src/main/java/Statistics.java
@@ -1,21 +1,60 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import objectJSON.Response;
+import objectJSON.Ticket;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Statistics {
-    public static long averageTime(List<Long> list) {
+    public static long averageTime(Response response, String departureCity, String arrivalCity) {
+        try {
+            List<Long> timeDiffList = listOfTimeDifference(response, departureCity, arrivalCity);
 
-        long sum = 0;
-        for (Long item : list) {
-            sum += item;
+            long sum = 0;
+            for (Long item : timeDiffList) {
+                sum += item;
+            }
+            long averageTimeMs = sum / timeDiffList.size();
+            System.out.println(timeConverter(averageTimeMs));
+            return averageTimeMs;
+
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-        long averageTimeMs = sum / list.size();
-        return averageTimeMs;
+        return 0;
     }
 
-    public static void percentile(List<Long> list) {
+    public static List<Long> listOfTimeDifference(Response response, String departureCity, String arrivalCity) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
 
+        Date departure_time = null;
+        Date arrival_time = null;
+
+        List<Long> timeDiffList = new ArrayList<>();
+
+        for (Ticket ticket : response.tickets) {
+            if (ticket.getOrigin_name().equals(departureCity) &
+                    ticket.getDestination_name().equals(arrivalCity)) {
+                departure_time = format.parse(ticket.getDeparture_time());
+                arrival_time = format.parse(ticket.getArrival_time());
+                //in milliseconds
+                long diff = arrival_time.getTime() - departure_time.getTime();
+                timeDiffList.add(diff);
+            }
+        }
+        return timeDiffList;
+    }
+
+    public static long percentile(Response response, int percentile, String departureCity, String arrivalCity) {
+        try {
+            List<Long> timeDiffList = listOfTimeDifference(response, departureCity, arrivalCity);
+            Collections.sort(timeDiffList);
+            int index = (int) Math.ceil((double) percentile / 100 * timeDiffList.size()) - 1;
+            return timeDiffList.get(index);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     public static Map<String, Long> timeConverter(long averageTimeMs) {
